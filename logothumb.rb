75a6f0a41d8ed190a19c4bdbf3dae0639bf6756d
Josh Headapohl <joshhead@gmail.com>
2012-01-07 22:45:02 -0500

    CamelCase to underscores

--- logothumb.rb
+++ logothumb.rb
@@ -1,55 +1,55 @@
-def getNewWidthHeight(boundsWidth, boundsHeight, padding, rectWidth, rectHeight)
-  # paddedWidth and paddedHeight: demensions once padding is removed on all sides
-  paddedWidth = boundsWidth - (2 * padding)
-  paddedHeight = boundsHeight - (2 * padding)
+def get_new_width_height(bounds_width, bounds_height, padding, rect_width, rect_height)
+  # padded_width and padded_height: demensions once padding is removed on all sides
+  padded_width = bounds_width - (2 * padding)
+  padded_height = bounds_height - (2 * padding)
 
-  boundsRatio = paddedWidth.to_f / paddedHeight
-  rectRatio = rectWidth.to_f / rectHeight
+  bounds_ratio = padded_width.to_f / padded_height
+  rect_ratio = rect_width.to_f / rect_height
 
-  if (boundsRatio > rectRatio)
+  if (bounds_ratio > rect_ratio)
     # pillarbox
-    newHeight = paddedHeight
-    newWidth = paddedHeight * rectRatio
+    new_height = padded_height
+    new_width = padded_height * rect_ratio
   else
     # letterbox
-    newHeight = paddedWidth / rectRatio
-    newWidth = paddedWidth
+    new_height = padded_width / rect_ratio
+    new_width = padded_width
   end
 
-  return [newWidth.to_i, newHeight.to_i]
+  return [new_width.to_i, new_height.to_i]
 end
 
 # Convenience method so fuzz property doesn't have to get messed up
-def getTrimmedImage(orig, fuzz=nil)
-  oldFuzz = orig.fuzz
+def get_trimmed_image(orig, fuzz=nil)
+  old_fuzz = orig.fuzz
   if (fuzz)
     orig.fuzz = fuzz
   end
   trimmed = orig.trim
-  orig.fuzz = oldFuzz
+  orig.fuzz = old_fuzz
   return trimmed
 end
 
 # Return an image that of width x height dimensions
 # Based on the average color from the rectangle at x, y
 # and of size w x h in the original image.
-def getBackgroundImage(orig, width, height, x, y, w, h)
+def get_background_image(orig, width, height, x, y, w, h)
   cropped = orig.crop(x, y, w, h)
-  onePx = cropped.scale(1,1)
-  background = onePx.scale(width, height)
+  one_px = cropped.scale(1,1)
+  background = one_px.scale(width, height)
   cropped.destroy!
-  onePx.destroy!
+  one_px.destroy!
   return background
 end
 
-def createThumbnail(blob, width, height, padding)
+def create_thumbnail(blob, width, height, padding)
   orig = Magick::ImageList.new.from_blob(blob)
-  trimmedImg = getTrimmedImage(orig, "2%")
-  scaledWidth, scaledHeight =
-    getNewWidthHeight(width, height, padding, trimmedImg.columns, trimmedImg.rows)
-  scaledImg = trimmedImg.resize(scaledWidth, scaledHeight)
-  bgImg = getBackgroundImage(orig, width, height, 0, 0, 3, 3)
-  thumbnail = bgImg.composite(scaledImg, Magick::CenterGravity, Magick::OverCompositeOp)
-  [orig, trimmedImg, scaledImg, bgImg].each { |img| img.destroy! }
+  trimmed_img = get_trimmed_image(orig, "2%")
+  scaled_width, scaled_height =
+    get_new_width_height(width, height, padding, trimmed_img.columns, trimmed_img.rows)
+  scaled_img = trimmed_img.resize(scaled_width, scaled_height)
+  bg_img = get_background_image(orig, width, height, 0, 0, 3, 3)
+  thumbnail = bg_img.composite(scaled_img, Magick::CenterGravity, Magick::OverCompositeOp)
+  [orig, trimmed_img, scaled_img, bg_img].each { |img| img.destroy! }
   return thumbnail
 end
