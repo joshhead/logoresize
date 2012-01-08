843a4dd4af4aefe33746d0844d3d1d8e00c5a306
Josh Headapohl <joshhead@gmail.com>
2012-01-08 17:17:35 -0500

    Foundation for clever bg sampling, set image format in different place

--- logothumb.rb
+++ logothumb.rb
@@ -32,6 +32,38 @@ module LogoThumb
     return trimmed
   end
 
+  # RMagick may have better ways to get at post-crop offset information.
+  # I couldn't find it, so I'm parsing it from the Image.inspect string.
+  # Will return nil if parsing fails.
+  def get_trim_properties(magick_string)
+    if (magick_string =~ /(\d+)x(\d+) (\d+)x(\d+)\+(\d+)\+(\d+)/)
+      prop = Hash.new
+      prop[:trimmed_width] = $1
+      prop[:trimmed_height] = $2
+      prop[:orig_width] = $3
+      prop[:orig_height] = $4
+      prop[:offset_x] = $5
+      prop[:offset_y] = $6
+      return prop
+    else
+      return nil
+    end
+  end
+
+  def get_bg_sample_rect(trim_properties)
+    rect = Hash.new
+    if (trim_properties.nil? || true)
+      # Make a blind guess.
+      # Any corner should be a reasonable bet
+      # If there was a border at all.
+      rect[:x] = 0
+      rect[:y] = 0
+      rect[:w] = 1
+      rect[:h] = 1
+    end
+    return rect
+  end
+
   # Return an image that of width x height dimensions
   # Based on the average color from the rectangle at x, y
   # and of size w x h in the original image.
@@ -53,13 +85,16 @@ module LogoThumb
     end
     orig = Magick::ImageList.new.from_blob(blob)
     trimmed_img = get_trimmed_image(orig, "2%")
+    trimmed_magick_string = trimmed_img.inspect
     scaled_width, scaled_height =
       get_new_width_height(width, height, padding, trimmed_img.columns, trimmed_img.rows)
     scaled_img = trimmed_img.resize(scaled_width, scaled_height)
     if (orig.rows == trimmed_img.rows && orig.columns == trimmed_img.columns)
-      bg_img = Magick::Image.new(width, height) {self.background_color = "transparent"}
+      bg_img = Magick::Image.new(width, height) {self.background_color = "transparent"; self.format = "PNG" }
     else
-      bg_img = get_background_image(orig, width, height, 0, 0, 3, 3)
+      rect = get_bg_sample_rect(get_trim_properties(trimmed_magick_string))
+      STDERR.puts rect.inspect
+      bg_img = get_background_image(orig, width, height, rect[:x], rect[:y], rect[:w], rect[:h])
     end
     thumbnail = bg_img.composite(scaled_img, Magick::CenterGravity, Magick::OverCompositeOp)
     [orig, trimmed_img, scaled_img, bg_img].each { |img| img.destroy! }
